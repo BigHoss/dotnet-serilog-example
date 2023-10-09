0e676b437e81171114d8f11b9c2b4cf62de6d69d
Raphael Kuster <raphael.kuster@neovac.ch>
2023-10-09 12:19:53 +0200

    feat: added demo project

---
+++ example.api/Extensions/LoggerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace example.api.Extensions;
+
+public static partial class LoggerExtensions
+{
+    private const int GeneralRange = 11100;
+
+    public static IDisposable? BeginMethodScope(this ILogger logger,
+        Dictionary<string, object?>? parameters = null,
+        [CallerMemberName] string methodName = "")
+    {
+        parameters ??= new();
+
+        parameters.Add("MethodName", methodName);
+        return logger.BeginScope(parameters);
+    }
+
+    [LoggerMessage(Level = LogLevel.Trace, EventId = GeneralRange + 0, Message = "method start")]
+    public static partial void TraceStart(this ILogger logger);
+
+    [LoggerMessage(Level = LogLevel.Trace, EventId = GeneralRange + 1, Message = "method end")]
+    public static partial void TraceEnd(this ILogger logger);
+}
